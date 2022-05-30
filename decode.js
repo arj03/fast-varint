7745fbba3aae5d063e17f44f81a7622ecbe35666
Anders Rune Jensen <arj03@protonmail.ch>
2022-05-30 22:27:01 +0200

    Faster decode

--- decode.js
+++ decode.js
@@ -1,29 +1,69 @@
 module.exports = read
 
-var MSB = 0x80
-  , REST = 0x7F
+const MSB = 0x80
+const REST = 0x7f
 
 function read(buf, offset) {
-  var res    = 0
-    , offset = offset || 0
-    , shift  = 0
-    , counter = offset
-    , b
-    , l = buf.length
+  offset = offset || 0
 
-  do {
-    if (counter >= l || shift > 49) {
-      read.bytes = 0
-      throw new RangeError('Could not decode varint')
-    }
-    b = buf[counter++]
-    res += shift < 28
-      ? (b & REST) << shift
-      : (b & REST) * Math.pow(2, shift)
-    shift += 7
-  } while (b >= MSB)
+  let b = buf[offset]
+  let res = 0
 
-  read.bytes = counter - offset
+  res += b & REST
+  if (b < MSB) {
+    read.bytes = 1
+    return res
+  }
 
-  return res
+  b = buf[offset + 1]
+  res += (b & REST) << 7
+  if (b < MSB) {
+    read.bytes = 2
+    return res
+  }
+
+  b = buf[offset + 2]
+  res += (b & REST) << 14
+  if (b < MSB) {
+    read.bytes = 3
+    return res
+  }
+  
+  b = buf[offset + 3]
+  res += (b & REST) << 21
+  if (b < MSB) {
+    read.bytes = 4
+    return res
+  }
+
+  b = buf[offset + 4]
+  res += (b & REST) * Math.pow(2, 28)
+  if (b < MSB) {
+    read.bytes = 5
+    return res
+  }
+
+  b = buf[offset + 5]
+  res += (b & REST) * Math.pow(2, 35)
+  if (b < MSB) {
+    read.bytes = 6
+    return res
+  }
+
+  b = buf[offset + 6]
+  res += (b & REST) * Math.pow(2, 42)
+  if (b < MSB) {
+    read.bytes = 7
+    return res
+  }
+
+  b = buf[offset + 7]
+  res += (b & REST) * Math.pow(2, 49)
+  if (b < MSB) {
+    read.bytes = 8
+    return res
+  }
+  
+  read.bytes = 0
+  throw new RangeError('Could not decode varint')
 }
